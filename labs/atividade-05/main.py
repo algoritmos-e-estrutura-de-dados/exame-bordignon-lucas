55dbde909c356b9b3aee7161446f58b7c14bb1a2
unknown <lucas.bordignon@edu.unifil.br>
2022-10-05 00:14:12 -0300

    exame figurinha final

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,12 +1,25 @@
-def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
 
+def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao):
+    
+    s = 0
+    r = 0
+    for figurinhas_da_maria in figurinhas_da_maria:
+        s += 1      
+        if figurinhas_da_maria not in figurinhas_da_joao:
+            y = figurinhas_da_joao
+            figurinhas_da_maria = y
+        else:
+            s -= 1 
+        if figurinhas_da_maria in figurinhas_da_joao:
+            r += 2
+    s -= r 
+    if s < 0:
+        s = 0                   
+    return s
 
 
 if __name__ == '__main__':
     A, B = input().split(' ')
     figurinhas_da_maria = input().split(' ')
     figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+    print(maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao))
